0748dea26f649b6c4b361efdbbfcc30a517f1350
Ashton Trey Belew <abelew@gmail.com>
2023-08-10 13:56:27 -0400

    moved the df merging function to fiji_tracker/xref_tracks.py Added an example invocation to the bottom of experiment 1647 along with an example of how to plot a boxplot of the observed areas in that experiment.

---
+++ fiji_tracker/xref_tracks.py
@@ -0,0 +1,39 @@
+import pandas
+import geopandas
+
+
+def xref_locations(first, second, first_x='X', first_y='Y', first_z='Slice',
+                   second_x='POSITION_X', second_y='POSITION_Y', second_z='POSITION_Z',
+                   max_dist=10, verbose=False):
+    pairwise_elements = pandas.DataFrame()
+    first_measurements = pandas.read_csv(first)
+    second_measurements = pandas.read_csv(second)
+    first_gdf = geopandas.GeoDataFrame(
+        first_measurements,
+        geometry=geopandas.points_from_xy(first_measurements[first_x],
+                                          first_measurements[first_y],
+                                          first_measurements[first_z]))
+    second_gdf = geopandas.GeoDataFrame(
+        second_measurements,
+        geometry=geopandas.points_from_xy(second_measurements[second_x],
+                                          second_measurements[second_y],
+                                          second_measurements[second_z]))
+    ti_rows = first_gdf.shape[0]
+    tj_rows = second_gdf.shape[0]
+    for ti_row in range(0, ti_rows):
+        if verbose:
+            print(f"On row: {ti_row}")
+        ti_element = first_gdf.iloc[[ti_row, ]]
+        titj = geopandas.sjoin_nearest(first_gdf, second_gdf,
+                                       distance_col="pairwise_dist",
+                                       max_distance=max_dist)
+        chosen_closest_dist = titj.pairwise_dist.min()
+        if (isnan(chosen_closest_dist)):
+            print(f"This element has no neighbor within {max_dist}.")
+        else:
+            chosen_closest_cell = titj.pairwise_dist == chosen_closest_dist
+            chosen_closest_row = titj[chosen_closest_cell]
+            pairwise_tmp = pandas.concat([pairwise_elements, chosen_closest_row])
+            pairwise_elements = pairwise_tmp
+            print(pairwise_elements.shape)
+    return pairwise_elements
